a95bcd49748992a2a27f76bd1cf40d6482cadee5
Nicolás Alvarez <nicolas.alvarez@gmail.com>
2017-07-26 02:18:09 -0300

    Test code to send a DM.

--- worker.py
+++ worker.py
@@ -6,10 +6,8 @@ import datetime
 import sys
 import os
 
-from tweepy.streaming import StreamListener
-from tweepy import OAuthHandler
-from tweepy import Stream
-from tweepy import API, Cursor
+from tweepy import API
+from tweepy.error import TweepError
 
 from model import Tweet, User, BeerCode
 from config import BotConfig
@@ -18,8 +16,9 @@ import time
 
 config = BotConfig("config.ini")
 
-# horrible
-#while True:
+auth = config.get_tweepy_auth()
+api = API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+
 for n in range(50):
 
     #get 1 twtit to process from database
@@ -47,8 +46,18 @@ for n in range(50):
         tweet.process_try=+1
 
         #Try to send the message to the user
-
-        #if success bla
+        # TODO: if the user doesn't follow us, but has open DMs, we *can* DM,
+        # but the message will appear in a separate tab and they may not see it.
+        # Maybe we should send a public reply telling them to check their DMs.
+        try:
+            dm = api.send_direct_message(tweet.user_id, text="Test message")
+        except TweepError as e:
+            # "You cannot send messages to users who are not following you."
+            if e.api_code == 150:
+                # TODO: tell user to follow us
+                pass
+            else:
+                raise
 
         #if it fails bla bla bla
         tweet.user.beers=+1
